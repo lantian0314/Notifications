3c44ea4b7d4c23fdd85027cbb119f4dd535515c0
384289339@qq.com <384289339@qq.com>
2015-08-10 15:22:36 +0800

    自己练习-通过布局来自定义通知栏内容的展示

--- src/com/example/notifications/tools/BaseTools.java
+++ src/com/example/notifications/tools/BaseTools.java
@@ -1,8 +1,12 @@
 package com.example.notifications.tools;
 
+import java.lang.ref.WeakReference;
+
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 public class BaseTools {
 	/**
@@ -27,5 +31,34 @@ public class BaseTools {
 		/*获取当前系统的android版本号*/
 		int version= android.os.Build.VERSION.SDK_INT;
 		return version;
+	}
+	
+	/**
+	 * 根据路径将图片转换为Bitmap
+	 * @param path
+	 * @param w
+	 * @param 
+	 * @return
+	 */
+	public static Bitmap convertToBitmap(String path, int w, int h) {
+	    BitmapFactory.Options opts = new BitmapFactory.Options();
+	// 设置为ture只获取图片大小
+	  opts.inJustDecodeBounds = true;
+	   opts.inPreferredConfig = Bitmap.Config.ARGB_8888;
+	   // 返回为空
+	  BitmapFactory.decodeFile(path, opts);
+	 int width = opts.outWidth;
+	  int height = opts.outHeight;
+	  float scaleWidth = 0.f, scaleHeight = 0.f;
+	  if (width > w || height > h) {
+	     // 缩放
+	     scaleWidth = ((float) width) / w;
+	     scaleHeight = ((float) height) / h;
+	 }
+	opts.inJustDecodeBounds = false;
+	  float scale = Math.max(scaleWidth, scaleHeight);
+	   opts.inSampleSize = (int)scale;
+	   WeakReference<Bitmap> weak = new WeakReference<Bitmap>(BitmapFactory.decodeFile(path, opts));
+	  return Bitmap.createScaledBitmap(weak.get(), w, h, true);
 	}
 }
---
+++ src/com/example/notifications/zx/BuJuNotifations.java
@@ -0,0 +1,59 @@
+package com.example.notifications.zx;
+
+
+import com.example.notifications.R;
+import com.example.notifications.tools.BaseTools;
+
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.NotificationCompat;
+import android.widget.RemoteViews;
+
+public class BuJuNotifations extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//获取状态通知栏的管理
+		NotificationManager mNotificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		//实例化通知栏管理器
+		NotificationCompat.Builder mBuilder=new NotificationCompat.Builder(this);
+		//先设定RemoteViews
+		RemoteViews view_custom = new RemoteViews(getPackageName(), R.layout.view_custom);
+		//设置对应IMAGEVIEW的ID的资源图片
+		//view_custom.setImageViewResource(R.id.custom_icon, R.drawable.icon);
+		String path=Environment.getExternalStorageDirectory().toString()+"/"+"123.png";    
+		Bitmap bitmap=BaseTools.convertToBitmap(path,40,40);
+		view_custom.setImageViewBitmap(R.id.custom_icon, bitmap);
+		view_custom.setTextViewText(R.id.tv_custom_title, "测试标题");
+		view_custom.setTextViewText(R.id.tv_custom_content, "测试内容");
+		//mBuilder.setContentTitle("测试标题");
+		//mBuilder.setContentText("测试内容");
+		mBuilder.setContentIntent(getDefalutIntent(Notification.FLAG_AUTO_CANCEL)); //设置通知栏点击意图  
+	//  .setNumber(number) //设置通知集合的数量  
+	    mBuilder.setTicker("测试通知来啦"); //通知首次出现在通知栏，带上升动画效果的  
+	   mBuilder .setWhen(System.currentTimeMillis());//通知产生的时间，会在通知信息里显示，一般是系统获取到的时间  
+	    mBuilder.setPriority(Notification.PRIORITY_DEFAULT); //设置该通知优先级  
+	//  .setAutoCancel(true)//设置这个标志当用户单击面板就可以让通知将自动取消    
+	    mBuilder.setOngoing(false);//ture，设置他为一个正在进行的通知。他们通常是用来表示一个后台任务,用户积极参与(如播放音乐)或以某种方式正在等待,因此占用设备(如一个文件下载,同步操作,主动网络连接)  
+	   mBuilder .setDefaults(Notification.DEFAULT_VIBRATE);//向通知添加声音、闪灯和振动效果的最简单、最一致的方式是使用当前的用户默认设置，使用defaults属性，可以组合  
+	    //Notification.DEFAULT_ALL  Notification.DEFAULT_SOUND 添加声音 // requires VIBRATE permission  
+	    mBuilder.setSmallIcon(R.drawable.ic_launcher);//设置通知小ICON  
+	    Notification notify = mBuilder.build();
+		notify.contentView = view_custom;
+	    mNotificationManager.notify(1, notify);
+	}
+
+	public PendingIntent getDefalutIntent(int flags){  
+	    PendingIntent pendingIntent= PendingIntent.getActivity(this, 1, new Intent(), flags);  
+	    return pendingIntent;  
+	}  
+
+}
